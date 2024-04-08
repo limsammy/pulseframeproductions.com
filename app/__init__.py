73eb72486d28c36f3ec7f7158eba82ae1b88a360
Sam Lim <sam@typhonic.io>
2024-04-08 05:55:12 -0700

    Rewrote app factory for new router core. Added some error handling

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,20 @@ from app.config import settings
 logger = get_logger(__name__)
 
 
-def create_app(title: str, description: str, version: str) -> FastAPI:
+def create_app(title: str, summary: str, description: str, version: str, debug: str) -> FastAPI:
     """App Factory for creating and returning FastAPI app instance"""
-    logger.debug("Creating FastAPI app instance")
+    logger.info("Creating FastAPI app instance")
+    logger.debug(
+        "Creating app with metadata: title={}, summary={}, description={}, version={}, debug={}".format(
+            title, summary, description, version, debug
+        )
+    )
     app = FastAPI(
         title=title,
+        summary=summary,
         description=description,
         version=version,
+        debug=bool(debug),
         docs_url=None,
         redoc_url=None,
         openapi_prefix=None,
@@ -25,20 +32,32 @@ def create_app(title: str, description: str, version: str) -> FastAPI:
     ####################
     # Register routers #
     ####################
-    logger.info("Importing API routers...")
-    from app.routers import ROUTERS
+    logger.info("Registering API routers...")
+    from app.routers import core
 
-    logger.info("Registering {} routers...".format(len(ROUTERS)))
-    for router in ROUTERS:
-        app.include_router(router, prefix=settings.API_VERSION_STRING)
-        logger.info("Registered router: {}".format(router.prefix))
-    logger.info("Done registering routers!")
+    logger.debug("Registering core router...")
+    app.include_router(core.router)
 
-    @app.get("/")
-    async def root():
-        return {"message": "Hello World"}
+    logger.info("Registered routers!")
 
-    logger.debug("Returniing FastAPI app instance")
+    # logger.info("Registering {} routers...".format(len(ROUTERS)))
+    # for router in ROUTERS:
+    #     app.include_router(router, prefix=settings.API_VERSION_STRING)
+    #     logger.info("Registered router: {}".format(router.prefix))
+    # logger.info("Done registering routers!")
+
+    # @app.get("/")
+    # async def root():
+    #     return {"message":
+    # "Hello World"}
+
+    logger.info("Checking if app instance is valid...")
+    if app is None:
+        logger.error("App instance is None!")
+        raise ValueError("App instance is None!")
+        return
+
+    logger.info("Everything looks good. Returniing FastAPI app instance")
     return app
 
 
