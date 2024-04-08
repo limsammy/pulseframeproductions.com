7bb18ad6e25aa778a4250e0368a44b86e3bb14eb
Sam Lim <sam@typhonic.io>
2024-04-08 10:23:19 -0700

    Do some basic pathlib ops in order to find templates dir. Additionally, register partials

--- app/routers/core.py
+++ app/routers/core.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from jinja_partials import register_starlette_extensions
 
 from app.utils.logger_utils import get_logger
 
@@ -20,10 +22,21 @@ router = APIRouter(
     tags=["core"],
     responses={404: {"description": "Not found"}},
 )
-logger.info("Created core_router!")
-logger.info("Creating Jinja2Templates instance...")
-templates = Jinja2Templates(directory="templates/")
-logger.info("Created Jinja2Templates instance!")
+
+# TODO: Move BASE_DIR to app/config.py
+BASE_DIR = Path(__file__).resolve().parent.parent
+logger.debug("Set BASE_DIR to: {}".format(BASE_DIR))
+
+# TODO: Shouold we move this to app/config.py?
+logger.debug("Setting Jinja2Templates directory to: {}".format(str(Path(BASE_DIR, "templates"))))
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
+logger.debug("Attempting to register jinja partials...")
+try:
+    register_starlette_extensions(templates)
+    logger.info("Registered jinja partials!")
+except Exception as error:
+    logger.error("Error registering jinja partials: {}".format(error))
+    raise error
 
 
 @router.get("/", response_class=HTMLResponse)
