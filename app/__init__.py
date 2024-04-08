bd597f2e462396c2a2d5f660e26bca4edf0e69b6
Sam Lim <sam@typhonic.io>
2024-04-08 04:25:15 -0700

    After setting up config.py the app factory is now designed to accept a lot of different metadata. Added that to the app factory (eg. title, description, version), and added a router registration block. Here we import all routers from our app.routers sub package and import each router module. So far there is just the single core_router that will serve as the router for the functionality of the main static site

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,28 @@ logger = get_logger(__name__)
 def create_app() -> FastAPI:
     """App Factory for creating and returning FastAPI app instance"""
     logger.debug("Creating FastAPI app instance")
-    app = FastAPI()
+    app = FastAPI(
+        title=title,
+        description=description,
+        version=version,
+        docs_url=None,
+        redoc_url=None,
+        openapi_prefix=None,
+        # openapi_tags=(),
+        # openapi_url="{}/openapi.json".format(settings.API_VERSION_STRING),
+    )
+
+    ####################
+    # Register routers #
+    ####################
+    logger.info("Importing API routers...")
+    from app.routers import ROUTERS
+
+    logger.info("Registering {} routers...".format(len(ROUTERS)))
+    for router in ROUTERS:
+        app.include_router(router, prefix=settings.API_VERSION_STRING)
+        logger.info("Registered router: {}".format(router.prefix))
+    logger.info("Done registering routers!")
 
     @app.get("/")
     async def root():
