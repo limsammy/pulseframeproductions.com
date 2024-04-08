5b16a701ad1125ce3d20cf37c2a0d7cb826186bd
Sam Lim <sam@typhonic.io>
2024-04-08 05:07:34 -0700

    Imported settings dict from config module, used settings.FASTAPI_APP_METADATA dict to populate new create_app() params

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from app import create_app, configure_static_files
 from app.utils.logger_utils import get_logger
+from app.config import settings
 
 logger = get_logger(__name__)
 
 
 logger.info("Creating FastAPI app instance...")
-app = create_app()
+app = create_app(**settings.FASTAPI_APP_METADATA)
 logger.info("Created FastAPI app instance!")
 
 logger.info("Configuring static files...")
