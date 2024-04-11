9a5ca726769475261240aae120a6cf23ca6b402f
Sam Lim <sam@typhonic.io>
2024-04-11 07:14:11 -0700

    Add another route for /works-carousel-roll. Realized works shjould be a resource with its own router eg /works/<id>

--- app/routers/works.py
+++ app/routers/works.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -11,15 +10,15 @@ logger = get_logger(__name__)
 
 tags_metadata = [
     {
-        "name": "core",
-        "description": "Core functionality for main website. This is very basic webapp functions such as GETs to /foo return a foo.html template.",
+        "name": "works",
+        "description": "Router for handling all 'Work' model and routing",
     }
 ]
 
 logger.info("Creating core_router...")
 router = APIRouter(
     # prefix="core",  # Since we do not have any other functionality other than static html files, omit prefix (I think omitting a prefix is the same as setting it to ")
-    tags=["core"],
+    tags=["works"],
     responses={404: {"description": "Not found"}},
 )
 
@@ -45,13 +44,3 @@ async def index(request: Request):
     logger.debug("Rendering index template...")
 
     return templates.TemplateResponse(request=request, name="index.html.jinja", context={"title": "PulseFrame | Home"})
-
-
-@router.get("/works-carousel-roll", response_class=HTMLResponse)
-async def works_carousel_roll(request: Request):
-    logger.info("Received GET request to /works-carousel-roll.html")
-    logger.debug("Rendering works-carousel-roll template...")
-
-    return templates.TemplateResponse(
-        request=request, name="works-carousel-roll.html.jinja", context={"title": "PulseFrame | Works Carousel Roll"}
-    )
